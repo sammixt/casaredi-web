fdb95b9b6ef0a6aef50e74c4e2792faaf82c2141
sezealaflw <samuel.ezeala@flutterwavego.com>
2023-02-09 08:33:49 +0100

    rate limiting

--- features/Property/api/getProperty.js
+++ features/Property/api/getProperty.js
@@ -1,7 +1,7 @@
-import { axios } from "@/lib/axios";
+import { makeRequest } from "@/lib/axios";
 
 export const getProperty = async(id) => {
-    const { data } = await axios.get('/properties/detail',{
+    const { data } = await makeRequest('/properties/detail',{
         params: {
            externalID: id
           }
--- features/common/api/getProperties.js
+++ features/common/api/getProperties.js
@@ -1,7 +1,7 @@
-import { axios } from "@/lib/axios";
+import {  makeRequest } from "@/lib/axios";
 
 export const getProperties = async(num) => {
-    const { data } = await axios.get('/properties/list',{
+    const { data } = await makeRequest('/properties/list',{
         params: {
             locationExternalIDs: '5002,6020',
             purpose: 'for-sale',
